a0df13830a4925d3fcfba6ac13f1ce6a71d90fa4
Nurullah <nurlan3295@gmail.com>
2019-01-20 17:03:01 +0400

    Add project files.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QacanLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // QacanLabel
+            // 
+            this.QacanLabel.AutoSize = true;
+            this.QacanLabel.Location = new System.Drawing.Point(110, 93);
+            this.QacanLabel.Name = "QacanLabel";
+            this.QacanLabel.Size = new System.Drawing.Size(65, 13);
+            this.QacanLabel.TabIndex = 0;
+            this.QacanLabel.Text = "QacanLabel";
+            this.QacanLabel.MouseHover += new System.EventHandler(this.QacanLabel_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.QacanLabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label QacanLabel;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void QacanLabel_MouseHover(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            QacanLabel.Top = random.Next(0, 400 - QacanLabel.Height);
+            QacanLabel.Left = random.Next(0, 400 - QacanLabel.Width);
+        }
+    }
+}
